05e9e7a1765f7895d55cbc8e21e83c0c2af87a33
Guillaume Nodet <gnodet@gmail.com>
2022-02-10 20:02:05 +0100

    Add header

--- src/main/java/org/eclipse/sisu/mojos/CachingWriter.java
+++ src/main/java/org/eclipse/sisu/mojos/CachingWriter.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2022-present Red Hat, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Guillaume Nodet (Red Hat, Inc.) - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.sisu.mojos;
 
 import java.io.IOException;
